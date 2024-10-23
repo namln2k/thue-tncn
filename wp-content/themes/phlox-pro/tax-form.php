cd7ce93efba60f808a9674d81174f8274aa2ed4d
namln2k <namln2aug2k@gmail.com>
2024-10-23 09:20:50 +0700

    Contact form v2

---
+++ wp-content/themes/phlox-pro/tax-form.php
@@ -0,0 +1,270 @@
+<?php
+function calculate($req)
+{
+  $numberOfIncomeSources = $req->get_param('number-of-income-sources');
+
+  $totalIncome = 0;
+  $totalTaxPaid = 0;
+  for ($i = 0; $i < $numberOfIncomeSources; $i++) {
+    $totalIncome += (int)$req->get_param("income-{$i}");
+    $totalTaxPaid += (int)$req->get_param("tax-paid-{$i}");
+  }
+
+  $personalDeduction = 132000000;
+
+  $numberOfDependents = (int)$req->get_param('number-of-dependents');
+  $dependentsDeduction = 52800000 * $numberOfDependents;
+
+  $charityDeduction = $req->get_param('charity-deduction');
+
+  $insuranceDeduction = $req->get_param('insurance-deduction');
+
+  $pensionDeduction = $req->get_param('pension-deduction');
+
+  $totalDeduction = $personalDeduction + $dependentsDeduction + $charityDeduction + $insuranceDeduction + $pensionDeduction;
+
+  $totalTaxableIncome = $totalIncome - $totalDeduction;
+
+  $totalTax = 0;
+  $level1FullTax = 60000000 * 5 / 100;
+  $level2FullTax = $level1FullTax + (120000000 - 60000000) * 10 / 100;
+  $level3FullTax = $level2FullTax + (216000000 - 120000000) * 15 / 100;
+  $level4FullTax = $level3FullTax + (384000000 - 216000000) * 20 / 100;
+  $level5FullTax = $level4FullTax + (624000000 - 384000000) * 25 / 100;
+  $level6FullTax = $level5FullTax + (960000000 - 624000000) * 30 / 100;
+  if ($totalTaxableIncome <= 60000000) {
+    $totalTax += $totalTaxableIncome * 5 / 100;
+  } elseif ($totalTaxableIncome <= 120000000) {
+    $totalTax += $level1FullTax;
+    $totalTax += ($totalTaxableIncome - 60000000) * 10 / 100;
+  } else if ($totalTaxableIncome <= 216000000) {
+    $totalTax += $level2FullTax;
+    $totalTax += ($totalTaxableIncome - 120000000) * 15 / 100;
+  } else if ($totalTaxableIncome <= 384000000) {
+    $totalTax += $level3FullTax;
+    $totalTax += ($totalTaxableIncome - 216000000) * 20 / 100;
+  } else if ($totalTaxableIncome <= 624000000) {
+    $totalTax += $level4FullTax;
+    $totalTax += ($totalTaxableIncome - 384000000) * 25 / 100;
+  } else if ($totalTaxableIncome <= 960000000) {
+    $totalTax += $level5FullTax;
+    $totalTax += ($totalTaxableIncome - 624000000) * 30 / 100;
+  } else {
+    $totalTax += $level6FullTax;
+    $totalTax += ($totalTaxableIncome - 960000000) * 35 / 100;
+  }
+
+  if ($totalTax - $totalTaxPaid > 0) {
+    $currentDatetime = current_datetime()->format('Y-m-d');
+    $settlementYear = $req->get_param('settlement-year');
+
+    switch ($settlementYear) {
+      case '2020':
+        $settlementYear = '2021-05-04';
+        break;
+      case '2021':
+        $settlementYear = '2022-05-04';
+        break;
+      case '2022':
+        $settlementYear = '2022-04-30';
+        break;
+      case '2023':
+        $settlementYear = '2023-05-02';
+        break;
+      default:
+        $settlementYear = "{$settlementYear}-04-30";
+    }
+
+    $latePaymentDays = subtract_dates($currentDatetime, $settlementYear);
+    $taxNeedToPay = $totalTax - $totalTaxPaid;
+
+    $warningLatePayment = $latePaymentDays * $taxNeedToPay * 0.03 / 100;
+
+    if ($latePaymentDays <= 5) {
+      $warningLatePaymentFine = [0, 0, 0];
+    } elseif ($latePaymentDays <= 30) {
+      $warningLatePaymentFine = ['1.000.000', '1.750.000', '2.500.000'];
+    } elseif ($latePaymentDays <= 60) {
+      $warningLatePaymentFine = ['2.500.000', '3.250.000', '4.000.000'];
+    } elseif ($latePaymentDays <= 90) {
+      $warningLatePaymentFine = ['4.000.000', '5.750.000', '7.500.000'];
+    } else {
+      $warningLatePaymentFine = [
+        thousand_seperate($taxNeedToPay),
+        thousand_seperate($taxNeedToPay * 1.5),
+        thousand_seperate($taxNeedToPay * 3)
+      ];
+    }
+
+    $refundableTax = 0;
+  } else {
+    $latePaymentDays = 0;
+    $taxNeedToPay = 0;
+    $refundableTax = $totalTaxPaid - $totalTax;
+  }
+
+  return [
+    'late_payment_days' => $latePaymentDays,
+    'tax_need_to_pay' => thousand_seperate($taxNeedToPay),
+    'warning_late_payment' => thousand_seperate($warningLatePayment),
+    'warning_late_payment_fine' => $warningLatePaymentFine,
+    'refundable_tax' => $refundableTax,
+    'total_income' => thousand_seperate($totalIncome) . 'Đ',
+    'personal_deduction' => thousand_seperate($personalDeduction) . 'Đ',
+    'dependents_deduction' => thousand_seperate($dependentsDeduction) . 'Đ',
+    'total_deduction' => thousand_seperate($totalDeduction) . 'Đ',
+    'total_taxable_income' => thousand_seperate($totalTaxableIncome) . 'Đ',
+    'total_tax' => thousand_seperate($totalTax) . 'Đ',
+    'total_tax_paid' => thousand_seperate($totalTaxPaid) . 'Đ'
+  ];
+}
+
+function calculate_tax($req)
+{
+  return json_encode([
+    'success' => true,
+    'data' => calculate($req)
+  ]);
+}
+
+function thousand_seperate($number)
+{
+  return number_format((int)$number, 0, '.', ',') . 'Đ';
+}
+
+function subtract_dates($date1, $date2)
+{
+  $datetime1 = strtotime($date1);
+  $datetime2 = strtotime($date2);
+
+  $secs = $datetime1 - $datetime2;
+  $days = $secs / 86400;
+
+  return $days;
+}
+
+function contact_calculate_tax($req)
+{
+  $mobile = $req->get_param('mobile-number');
+
+  $to = trim(get_bloginfo('admin_email'));
+  $subject = "Có khách hàng liên hệ điền phiếu tính thuế";
+  $body = "";
+  $body .= "
+    <div>
+        <ul>
+            <li><p>Số điện thoại: {$mobile}</p></li>";
+
+  $customerName = $req->get_param('customer-name');
+  $body .= "<li><p>Họ tên: {$customerName}</p></li>";
+
+  $customerIssue = $req->get_param('customer-issue');
+  $body .= "<li><p>Vấn đề: {$customerIssue}</p></li>";
+
+  $settlementYear = $req->get_param('settlement-year');
+  $body .= "<li><p>Năm quyết toán: {$settlementYear}</p></li>";
+
+  $numberOfIncomeSources = $req->get_param('number-of-income-sources');
+  $body .= "
+    <li>
+        <p>Các nguồn thu nhập</p>
+        <ul>";
+
+  for ($i = 0; $i < $numberOfIncomeSources; $i++) {
+    $index = $i + 1;
+    $income = $req->get_param("income-{$i}");
+    $income = thousand_seperate($income);
+    $taxPaid = $req->get_param("tax-paid-{$i}");
+    $taxPaid = thousand_seperate($taxPaid);
+
+    $body .= "
+            <li><p>Nguồn {$index}:</p>
+                <ul>
+                    <li>Tổng thu nhập: {$income}</li>
+                    <li>Số thuế đã khấu trừ: {$taxPaid}</li>
+                </ul>
+            </li>";
+  }
+
+  $body .= "
+        </ul>
+    </li>";
+
+  $calculatedResult = calculate($req);
+
+  $totalIncome = $calculatedResult['total_income'];
+  $body .= "<li><p>Tổng thu nhập chịu thuế: {$totalIncome}</p></li>";
+
+  $numberOfDependents = $req->get_param('number-of-dependents');
+  $body .= "<li><p>Số người phụ thuộc: {$numberOfDependents}</p></li>";
+
+  $personalDeduction = $calculatedResult['personal_deduction'];
+  $body .= "<li><p>Giảm trừ cho bản thân cá nhân: {$personalDeduction}</p></li>";
+
+  $dependentsDeduction = $calculatedResult['dependents_deduction'];
+  $body .= "<li><p>Giảm cho những người phụ thuộc được giảm trừ: {$dependentsDeduction}</p></li>";
+
+  $charityDeduction = $req->get_param('charity-deduction');
+  $charityDeduction = thousand_seperate($charityDeduction);
+  $body .= "<li><p>Tổng từ thiện nhân đạo khuyến học được trừ: {$charityDeduction}</p></li>";
+
+  $insuranceDeduction = $req->get_param('insurance-deduction');
+  $insuranceDeduction = thousand_seperate($insuranceDeduction);
+  $body .= "<li><p>Tổng các khoản đóng bảo hiểm được trừ: {$insuranceDeduction}</p></li>";
+
+  $pensionDeduction = $req->get_param('pension-deduction');
+  $pensionDeduction = thousand_seperate($pensionDeduction);
+  $body .= "<li><p>Tổng các khoản đóng quỹ HTTN được trừ: {$pensionDeduction}</p></li>";
+
+  $totalDeduction = $calculatedResult['total_deduction'];
+  $body .= "<li><p>Tổng các khoản giảm trừ: {$totalDeduction}</p></li>";
+
+  $totalTaxableIncome = $calculatedResult['total_taxable_income'];
+  $body .= "<li><p>Tổng thu nhập tính thuế: {$totalTaxableIncome}</p></li>";
+
+  $totalTax = $calculatedResult['total_tax'];
+  $body .= "<li><p>Tổng số thuế TNCN phát sinh trong kỳ: {$totalTax}</p></li>";
+
+  $totalTaxPaid = $calculatedResult['total_tax_paid'];
+  $body .= "<li><p>Tổng số thuế đã nộp trong kỳ: {$totalTaxPaid}</p></li>";
+
+  $latePaymentDays = $calculatedResult['late_payment_days'];
+  $body .= "<li><p>Số ngày chậm quyết toán: {$latePaymentDays}</p></li>";
+
+  $taxNeedToPay = $calculatedResult['tax_need_to_pay'];
+  $body .= "<li><p>Tổng số tiền thuế phải nộp thêm trong kỳ: {$taxNeedToPay}</p></li>";
+
+  $warningLatePayment = $calculatedResult['warning_late_payment'];
+  $body .= "<li><p>\"Cảnh báo\" phát sinh tổng số tiền chậm nộp: {$warningLatePayment}</p></li>";
+
+  $warningLatePaymentFine = implode(" / ", $calculatedResult['warning_late_payment_fine']);
+  $body .= "<li><p>\"Cảnh báo\" có thể phát sinh tiền phạt theo quy định: {$warningLatePaymentFine}</p></li>";
+
+  $refundableTax = $calculatedResult['refundable_tax'];
+  $body .= "<li><p>Tổng số tiền thuế được hoàn lại trong kỳ: {$refundableTax}</p></li>";
+
+  $body .= "
+    </ul>
+        </div>";
+
+  $headers = array("Content-Type: text/html; charset=UTF-8", "From: My Site Name <support@example.com>");
+
+  wp_mail($to, $subject, $body, $headers);
+
+  if (validate_mobile_number($mobile)) {
+    return json_encode([
+      'success' => true,
+      'message' => 'Cám ơn bạn đã liên hệ, chúng tôi sẽ liên lạc lại bạn sớm nhất.'
+    ]);
+  }
+
+  return json_encode([
+    'success' => false,
+    'message' => 'Vui lòng nhập số điện thoại.'
+  ]);
+}
+
+function validate_mobile_number($mobile)
+{
+  return preg_match('/^\d+$/', $mobile);
+}
--- wp-content/themes/phlox-pro/templates/tax-form.php
+++ wp-content/themes/phlox-pro/templates/tax-form.php
@@ -96,7 +96,7 @@
     padding-left: 30px;
   }
 
-  .tax-form .input[disabled] {
+  .tax-form .input[readonly] {
     background-color: var(--e-global-color-text);
   }
 
@@ -153,6 +153,12 @@
   .result .contact-form .form {
     display: flex;
     margin-bottom: 12px;
+    flex-direction: column;
+    gap: 12px;
+  }
+
+  .result .contact-form .form .label {
+    color: #FFF;
   }
 
   .result .contact-form p {
@@ -162,27 +168,24 @@
     text-align: center;
   }
 
-  .result .contact-form input[type=tel] {
+  .result .contact-form input,
+  .result .contact-form textarea {
     font-size: 16px;
     border-radius: 8px;
     background-color: #FFFFFF;
     color: #5C5C5C;
     padding: 13px 0px 13px 22px;
     border: none;
-    -webkit-box-shadow: none;
     box-shadow: none;
     outline: none;
+    width: 370px;
   }
 
-  .result .contact-form input[type=tel]:focus {
+  .result .contact-form input:focus {
     border: none;
     -webkit-box-shadow: none;
     box-shadow: none;
     outline: none;
-  }
-
-  .result .contact-form input[type=tel]::placeholder {
-    font-style: italic;
   }
 
   .result .contact-form button[type=submit] {
@@ -224,9 +227,14 @@
       <fieldset class="fieldset row">
         <field class="field required">
           <label for="settlement-year" class="label">Năm quyết toán</label>
-          <select name="settlement-year" id="settlement-year" class="input select" required>
-            <?php foreach (range(2010, 2024) as $year) {
-              echo "<option value='$year'>$year</option>";
+          <select name="settlement-year" id="settlement-year" class="input select" value=2024 required>
+            <option value='2019'>Trước năm 2019</option>
+            <?php foreach (range(2018, 2024) as $year) {
+              if ($year === 2024) {
+                echo "<option value='$year' selected>$year</option>";
+              } else {
+                echo "<option value='$year'>$year</option>";
+              }
             } ?>
           </select>
         </field>
@@ -256,7 +264,7 @@
       <fieldset class="fieldset row">
         <field class="field">
           <label for="total-income" class="label">Tổng thu nhập chịu thuế</label>
-          <input type="text" id="total-income" name="total-income" class="input text" disabled>
+          <input type="text" id="total-income" name="total-income" class="input text" readonly>
         </field>
         <field class="field required">
           <label for="number-of-dependents" class="label">Số người phụ thuộc</label>
@@ -271,11 +279,11 @@
       <fieldset class="fieldset row">
         <field class="field">
           <label for="personal-deduction" class="label">Giảm trừ cho bản thân cá nhân</label>
-          <input type="text" id="personal-deduction" name="personal-deduction" class="input text" disabled>
+          <input type="text" id="personal-deduction" name="personal-deduction" class="input text" readonly>
         </field>
         <field class="field">
           <label for="dependants-deduction" class="label">Giảm cho những người phụ thuộc được giảm trừ</label>
-          <input type="text" id="dependants-deduction" name="dependants-deduction" class="input text" disabled>
+          <input type="text" id="dependants-deduction" name="dependants-deduction" class="input text" readonly>
         </field>
       </fieldset>
       <fieldset class="fieldset row">
@@ -295,23 +303,23 @@
         </field>
         <field class="field">
           <label for="total-deduction" class="label">Tổng các khoản giảm trừ</label>
-          <input type="text" id="total-deduction" name="total-deduction" class="input text" disabled>
+          <input type="text" id="total-deduction" name="total-deduction" class="input text" readonly>
         </field>
       </fieldset>
       <fieldset class="fieldset row">
         <field class="field">
           <label for="total-taxable-income" class="label">Tổng thu nhập tính thuế</label>
-          <input type="text" id="total-taxable-income" name=" name=" pension-deduction"" class="input text" disabled>
+          <input type="text" id="total-taxable-income" name="total-taxable-income" class="input text" readonly>
         </field>
         <field class="field">
           <label for="total-tax" class="label">Tổng số thuế TNCN phát sinh trong kỳ</label>
-          <input type="text" id="total-tax" name="total-tax" class="input text" disabled>
+          <input type="text" id="total-tax" name="total-tax" class="input text" readonly>
         </field>
       </fieldset>
       <fieldset class="fieldset row">
         <field class="field">
           <label for="total-tax-paid" class="label">Tổng số thuế đã nộp trong kỳ</label>
-          <input type="text" id="total-tax-paid" name="pension-deduction" class="input text" disabled>
+          <input type="text" id="total-tax-paid" name="total-tax-paid" class="input text" readonly>
         </field>
       </fieldset>
       <div class="footer">
@@ -334,27 +342,27 @@
           <tr class="row">
             <td class="title">Tổng số tiền thuế phải nộp thêm trong kỳ</td>
             <td class="content">
-              <p><b><span id="tax-need-to-pay"></span>Đ</b></p>
+              <p><b><span id="tax-need-to-pay"></span></b></p>
             </td>
           </tr>
           <tr class="row">
             <td class="title">"Cảnh báo" phát sinh tổng số tiền chậm nộp</td>
             <td class="content">
-              <p><b><span id="warning-late-payment"></span>Đ</b></p>
+              <p><b><span id="warning-late-payment"></span></b></p>
             </td>
           </tr>
           <tr class="row">
             <td class="title">"Cảnh báo" có thể phát sinh tiền phạt theo quy định</td>
             <td class="content">
-              <p>Mức thấp nhất: <b><span id="warning-late-payment-fine-0"></span>Đ</b></p>
-              <p>Mức trung bình: <b><span id="warning-late-payment-fine-1"></span>Đ</b></p>
-              <p>Mức cao nhất: <b><span id="warning-late-payment-fine-2"></span>Đ</b></p>
+              <p>Mức thấp nhất: <b><span id="warning-late-payment-fine-0"></span></b></p>
+              <p>Mức trung bình: <b><span id="warning-late-payment-fine-1"></span></b></p>
+              <p>Mức cao nhất: <b><span id="warning-late-payment-fine-2"></span></b></p>
             </td>
           </tr>
           <tr class="row">
             <td class="title">Tổng số tiền thuế được hoàn lại trong kỳ</td>
             <td class="content">
-              <p><b><span id="refundable-tax"></span>Đ<b></p>
+              <p><b><span id="refundable-tax"></span><b></p>
             </td>
           </tr>
         </tbody>
@@ -363,7 +371,18 @@
     <div class="contact-form">
       <p>Các thông tin nêu trên chỉ là dự tính. Nếu bạn cần làm rõ thêm, hoặc cần tư vấn đề giảm thiểu vi phạm, giảm mức phạt theo luật, tối ưu tăng mức thuế được hoàn theo luật, vui lòng đăng ký tư vấn.</p>
       <form action="/?rest_route=/api/v1/contact-calculate-tax" method="POST" class="form" id="contact-form">
-        <input size="40" maxlength="400" class="" placeholder="Số điện thoại" value="" type="tel" name="mobile-number">
+        <field class="field required">
+          <label for="customer-name" class="label">Họ tên</label>
+          <input type="text" id="customer-name" name="customer-name" class="input text" required>
+        </field>
+        <field class="field required">
+          <label for="mobile-number" class="label">Số điện thoại</label>
+          <input size="40" maxlength="400" type="tel" name="mobile-number" required>
+        </field>
+        <field class="field">
+          <label for="customer-issue" class="label">Vấn đề</label>
+          <textarea id="customer-issue" name="customer-issue" rows=5></textarea>
+        </field>
         <button class="submit" type="submit">
           <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M15.018 6.2792C14.9102 6.32145 14.841 6.38095 14.7702 6.43268C13.8153 7.13021 12.8629 7.83292 11.9063 8.52872C10.6575 9.43664 9.40629 10.3394 8.15669 11.2464C8.10153 11.2869 8.05461 11.2852 7.99452 11.2645C6.45515 10.7274 4.91413 10.1919 3.37476 9.65391C2.55321 9.3668 1.73331 9.07364 0.911767 8.78653C0.435962 8.62098 0.120679 8.30024 0.0227192 7.77687C-0.0826495 7.21471 0.185711 6.6603 0.677981 6.39992C0.932347 6.26541 1.20894 6.19557 1.47977 6.11021C2.40422 5.81878 3.31467 5.48252 4.23417 5.17557C5.50765 4.7505 6.78113 4.32456 8.05132 3.88915C8.52795 3.72619 9.01116 3.58478 9.4845 3.41148C10.6114 2.99848 11.7598 2.6579 12.8917 2.26042C13.6581 1.99141 14.4335 1.75085 15.1983 1.47925C16.0256 1.1861 16.8669 0.941233 17.6917 0.64032C18.2523 0.435975 18.8244 0.27043 19.3867 0.0703957C20.1465 -0.20034 20.8396 0.347167 20.9796 1.07488C21.0158 1.26198 21.0026 1.44304 20.9499 1.62497C20.6758 2.57599 20.3613 3.51408 20.0806 4.46252C19.7382 5.61961 19.376 6.76981 19.0212 7.92259C18.5849 9.34093 18.1511 10.7601 17.7098 12.1759C17.3534 13.3209 17.0101 14.4694 16.6578 15.6161C16.2174 17.0483 15.7737 18.4787 15.3325 19.91C15.2016 20.335 15.0954 20.7687 14.9414 21.1852C14.7538 21.693 14.301 22.0034 13.7733 22C13.2868 21.9974 12.825 21.6697 12.6554 21.1722C12.4291 20.5101 12.2224 19.8401 12.0109 19.1719C11.4231 17.3216 10.8378 15.4704 10.2476 13.6201C10.2155 13.5184 10.2328 13.4511 10.2888 13.3666C11.1885 12.0259 12.0841 10.6817 12.9814 9.33921C13.6474 8.34249 14.3133 7.34577 14.9793 6.34904C14.9892 6.33439 14.9966 6.31887 15.0196 6.27662L15.018 6.2792Z" fill="white" />
